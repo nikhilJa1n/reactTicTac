74342a733cd499ef4daf5496df2bbd9ee1735c3c
nikhilja1n <nikhilj50@gmail.com>
2019-07-08 23:12:40 +0530

    used constructor and map to making box

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      board: Array(9).fill('X'),
+    };
+  }
+
   render() {
+    const Box = this.state.board.map(box => (
+      <div className="square">{box}</div>
+    ));
     return (
       <div className="container">
         <h1>Tic Tac Toe</h1>
-        <div className="board">
-          <div className="square" />
-          <div className="square" />
-          <div className="square" />
-          <div className="square" />
-          <div className="square" />
-          <div className="square" />
-          <div className="square" />
-          <div className="square" />
-          <div className="square" />
-        </div>
+        <div className="board">{Box}</div>
       </div>
     );
   }
